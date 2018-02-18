37154815c08dfcc1c06642d3b1ae8e31844b1788
António Pinho <antonio.henrique@ua.pt>
2018-02-18 15:28:17 +0000

    Commit fourth

--- app/src/main/java/pt/sysdeveloper/carteiradeclientes/ActCadCliente.java
+++ app/src/main/java/pt/sysdeveloper/carteiradeclientes/ActCadCliente.java
@@ -3,7 +3,11 @@ package pt.sysdeveloper.carteiradeclientes;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class ActCadCliente extends AppCompatActivity {
     /*Componentes*/
@@ -26,4 +30,30 @@ public class ActCadCliente extends AppCompatActivity {
         edtTelefone = (EditText)findViewById(R.id.edtTelefone);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_act_card_cliente, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    /*subescrever método*/
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.action_ok:
+                Toast.makeText(this, "Botão OK Selecionado", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_cancelar:
+                Toast.makeText(this, "Botão CANCELAR Selecionado", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
