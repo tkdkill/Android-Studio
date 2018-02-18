36820d419603cb395431df013865ffc61e05469d
António Pinho <antonio.henrique@ua.pt>
2018-02-18 22:55:25 +0000

    Commit seven

--- app/src/main/java/pt/sysdeveloper/carteiradeclientes/ActMain.java
+++ app/src/main/java/pt/sysdeveloper/carteiradeclientes/ActMain.java
@@ -1,17 +1,29 @@
 package pt.sysdeveloper.carteiradeclientes;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import pt.sysdeveloper.carteiradeclientes.database.DadosOpenHelper;
+
 public class ActMain extends AppCompatActivity {
 
     private RecyclerView lstDados;
     private FloatingActionButton fab;
+    private ConstraintLayout layoutContentMain;
+
+    private SQLiteDatabase conexao;
+
+    private DadosOpenHelper dadosOpenHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +34,25 @@ public class ActMain extends AppCompatActivity {
 
         fab = (FloatingActionButton) findViewById(R.id.fab);
         lstDados = (RecyclerView)findViewById(R.id.lstDados);
-        /*fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        layoutContentMain = (ConstraintLayout)findViewById(R.id.layoutContentMain);
 
+        criatConexao();
+    }
 
+    private void criatConexao(){
+        try {
+            dadosOpenHelper = new DadosOpenHelper(this);
+            conexao = dadosOpenHelper.getWritableDatabase();
+            Snackbar.make(layoutContentMain, R.string.message_conexao_criada_com_sucesso, Snackbar.LENGTH_LONG).setAction(R.string.action_ok, null).show();
 
-            }
-        });*/
+        }catch (SQLiteException ex){
+            AlertDialog.Builder dlg = new AlertDialog.Builder(this);
+            dlg.setTitle(R.string.title_erro);
+            dlg.setMessage(ex.getMessage());
+            dlg.setNeutralButton(R.string.action_ok, null);
+            dlg.show();
+
+        }
     }
 
     public void registar(View View){
