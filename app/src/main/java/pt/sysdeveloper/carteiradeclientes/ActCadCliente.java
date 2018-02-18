efc5b4f1739ec20b3cf9ed6302de175dd70aeb76
António Pinho <antonio.henrique@ua.pt>
2018-02-18 13:09:23 +0000

    Commit third

--- app/src/main/java/pt/sysdeveloper/carteiradeclientes/ActCadCliente.java
+++ app/src/main/java/pt/sysdeveloper/carteiradeclientes/ActCadCliente.java
@@ -3,8 +3,14 @@ package pt.sysdeveloper.carteiradeclientes;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.EditText;
 
 public class ActCadCliente extends AppCompatActivity {
+    /*Componentes*/
+    private EditText edtNome;
+    private EditText edtEndereco;
+    private EditText edtEmail;
+    private EditText edtTelefone;
 
 
     @Override
@@ -13,6 +19,11 @@ public class ActCadCliente extends AppCompatActivity {
         setContentView(R.layout.act_cad_cliente);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        edtNome     = (EditText)findViewById(R.id.edtNome);
+        edtEndereco = (EditText)findViewById(R.id.edtEndereco);
+        edtEmail    = (EditText)findViewById(R.id.edtEmail);
+        edtTelefone = (EditText)findViewById(R.id.edtTelefone);
     }
 
 }
