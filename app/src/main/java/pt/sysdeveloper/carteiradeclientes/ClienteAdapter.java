33c1db6000eabb99d49da056da74c8aff94a23de
António Pinho <antoniopinho@sysdeveloper.pt>
2018-03-04 22:20:48 +0000

    Commit sixteen

--- app/src/main/java/pt/sysdeveloper/carteiradeclientes/ClienteAdapter.java
+++ app/src/main/java/pt/sysdeveloper/carteiradeclientes/ClienteAdapter.java
@@ -1,27 +1,50 @@
 package pt.sysdeveloper.carteiradeclientes;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import pt.sysdeveloper.carteiradeclientes.dominio.entidades.Cliente;
 
 /**
  * Created by António - Local on 04/03/2018.
  */
 
-public class ClienteAdapter extends RecyclerView.Adapter {
+public class ClienteAdapter extends RecyclerView.Adapter<ClienteAdapter.ViewHolderCliente> {
 
+    private List<Cliente> dados;
+
+    public ClienteAdapter(List<Cliente> dados) {
+        this.dados = dados;
+    }
 
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public ClienteAdapter.ViewHolderCliente onCreateViewHolder(ViewGroup parent, int viewType) {
         return null;
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(ClienteAdapter.ViewHolderCliente holder, int position) {
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return dados.size();
+    }
+
+    public class ViewHolderCliente extends RecyclerView.ViewHolder{
+
+        public TextView txtNome;
+        public TextView txtTelefone;
+
+        public ViewHolderCliente(View itemView){
+            super(itemView);
+            txtNome     = (TextView) itemView.findViewById(R.id.textNome);
+            txtTelefone = (TextView) itemView.findViewById(R.id.edtTelefone);
+        }
     }
 }
