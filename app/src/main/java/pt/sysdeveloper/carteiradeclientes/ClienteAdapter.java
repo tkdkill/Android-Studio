4b8e47ad2130c91f832808b3f512d271a0fe0a33
António Pinho <antonio.henrique@ua.pt>
2018-03-04 19:06:57 +0000

    Commit fifth

---
+++ app/src/main/java/pt/sysdeveloper/carteiradeclientes/ClienteAdapter.java
@@ -0,0 +1,27 @@
+package pt.sysdeveloper.carteiradeclientes;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by António - Local on 04/03/2018.
+ */
+
+public class ClienteAdapter extends RecyclerView.Adapter {
+
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
